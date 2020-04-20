967a732954033b231f64d57dbbcf24ab77530f4b
Luis Revilla <luisrevillameza@gmail.com>
2020-04-20 13:23:18 -0400

    add props interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { ReactComponentElement } from 'react';
 import './App.css';
+
+
+interface MyComponentProps {
+  name: string,
+  amount: number,
+}
+
+function MyComponent({ name, amount }: MyComponentProps) {
+  return (
+    <div>
+      hello {name}!
+
+      this is your amount: {amount}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
-      <div className="flex-shrink-0">
-        <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"></img>
-      </div>
-      <div className="ml-6 pt-1">
-        <h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
-        <p className="text-base text-gray-600 leading-normal">You have a new message!</p>
-      </div>
+    <div>
+      <MyComponent name="Luis"/>
     </div>
   );
 }
