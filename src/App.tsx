0fc467b4aa79d091582b0ff94e445e9d3131ac21
Luis Revilla <luisrevillameza@gmail.com>
2020-04-20 10:11:11 -0400

    set up tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+      <div className="flex-shrink-0">
+        <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"></img>
+      </div>
+      <div className="ml-6 pt-1">
+        <h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
+        <p className="text-base text-gray-600 leading-normal">You have a new message!</p>
+      </div>
     </div>
   );
 }
