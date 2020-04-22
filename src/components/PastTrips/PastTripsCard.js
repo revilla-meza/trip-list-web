6426cf4f35994884610671b46ffea9c846672863
Pedro Revilla <peterevilla@gmail.com>
2020-04-22 16:01:36 -0500

    wip

--- src/components/PastTrips/PastTripsCard.js
+++ src/components/PastTrips/PastTripsCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
+
+const bg = {
+  backgroundImage: 'url("https://images.unsplash.com/photo-1564186763535-ebb21ef5277f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")',
+};
+
 const PastTripsCard = ({ trips }) => {
   return (
-    <div className="p-1">
-      <div className="flex items-center justify-between border-black bg-blue-200 rounded p-1 hover:bg-red-100">
-        <p className="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{trips.destination}</p>
-        <p className="text-gray-600 italic text-xs">{trips.name}</p>
-        <div>
-          <p className="self-end text-xs ml-16 text-gray-600 font-semibold font-sans tracking-wide">
-            {trips.created_at}
-          </p>
-        </div>
+    <div className="px-6 my-2 ">
+      <div className="flex flex-col  rounded p-1 shadow-lg" >
+        <p className="text-black font-semibold font-sans tracking-wide">{trips.destination}</p>
+        <p className="text-gray-700 italic text-xs">{trips.name}</p>
+        <p className="text-gray-600 italic text-xs self-end">{trips.created_at}</p>
       </div>
     </div>
   );
