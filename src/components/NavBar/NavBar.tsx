33ddb4f6b9291961f5a95923a8ebfe60021ca613
Pedro Revilla <47826392+peterevilla@users.noreply.github.com>
2020-04-29 16:40:00 -0500

    Tl 25: Improve Navbar implementation (#8)
    * TL-25: Improve NavBar Implementation
    
    * TL-25: Improve NavBar Implementation
    
    * TL-25: Improve NavBar Implementation
    
    * TL-25: Improve NavBar Implementation
    
    * migrate to ts; fix linting
    
    Co-authored-by: Pedro Revilla <peterevilla@pedros-air.attlocal.net>
    Co-authored-by: Luis Revilla <luisrevillameza@gmail.com>

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import add from './add.svg';
+import past from './past.svg';
+import items from './items.svg';
+
+const NavBar = () => {
+  const navBarButtons = [
+    {
+      path: '/past',
+      iconSrc: past,
+      label: 'my trips',
+    },
+    {
+      path: '/add',
+      iconSrc: add,
+      label: 'add trip',
+    },
+    {
+      path: '/items',
+      iconSrc: items,
+      label: 'my items',
+    },
+  ];
+
+  return (
+    <div className="flex flex-row justify-around border-gray-500 border-t inset-x-0 bottom-0 absolute fixed h-16 text-center">
+      {navBarButtons.map(({ iconSrc, label, path }) => (
+        <NavBarButton iconSrc={iconSrc} label={label} path={path} key={path} />
+      ))}
+    </div>
+  );
+};
+
+export default NavBar;
+
+//Nav Bar Buttons
+const NavBarButton = ({ iconSrc, label, path }: any) => {
+  return (
+    <Link to={path} className="flex flex-col mt-2 justify-end">
+      <img src={iconSrc} alt="nav-bar button" />
+      <span>{label}</span>
+    </Link>
+  );
+};
