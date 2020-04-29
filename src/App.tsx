ad3797f80a5d709f73c942a7cce15c07b54b8c7c
Pedro Revilla <peterevilla@pedros-air.attlocal.net>
2020-04-29 10:08:38 -0500

    TL-24: Implement Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,37 @@ import PastTripList from './components/PastTrips/PastTripsList';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import TripPage from './components/Trips/TripPage';
 import AddTrip from './components/CreateTrip/AddTrip';
-import NavBar from './components/NavBar/NavBar'
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div>
       <Switch>
         <Redirect exact from="/" to="past" />
-        <Route path="/past" render={() => { return (<div><PastTripList/><NavBar/></div>) }} />
-        <Route path="/trip/:id" render={() => { return (<div><TripPage/><NavBar/></div>) }} />
-        <Route exact path="/add" component={AddTrip} />  
-      </Switch>   
+        <Route
+          path="/past"
+          render={() => {
+            return (
+              <div>
+                <PastTripList />
+                <NavBar />
+              </div>
+            );
+          }}
+        />
+        <Route
+          path="/trip/:id"
+          render={() => {
+            return (
+              <div>
+                <TripPage />
+                <NavBar />
+              </div>
+            );
+          }}
+        />
+        <Route exact path="/add" component={AddTrip} />
+      </Switch>
     </div>
   );
 }
