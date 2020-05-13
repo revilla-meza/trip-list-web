20aa8f3cd66003776799a8b7ba8202cd44651af3
Pedro Revilla <peterevilla@pedros-air.attlocal.net>
2020-05-13 15:06:00 -0500

    TL-39: Create trip page connected via redux

--- src/routes/AddTrip/index.tsx
+++ src/routes/AddTrip/index.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SVGIcon from '../../components/SVGIcon';
+import { connect } from 'react-redux';
+import createTrip from '../../actions/createTrip';
 
-const AddTrip = () => {
-  // ADD TRIP FORM
+interface ComponentStateProps {
+  createTrip: any;
+  status: any;
+}
+
+type AppState = ComponentStateProps;
+
+const AddTrip = ({ createTrip, status }: AppState) => {
+  const [newTrip, setNewTrip] = useState({ destination: '', title: '', travel_method: '' });
+
+  const onChangeHandler = (e: any) => {
+    setNewTrip({ ...newTrip, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitHandler = (e: any) => {
+    e.preventDefault();
+    createTrip(newTrip);
+    e.target.reset();
+  };
   return (
     <div>
       <Link to="/past" className="">
         <SVGIcon icon="ArrowBack" />
       </Link>
       <h1 className="mt-4 mb-16 ml-8 font-sans text-lg font-bold ">Create your Trip</h1>
-      <form className="px-6 my-2 ">
+      <form onSubmit={onSubmitHandler} className="px-6 my-2 ">
         <label>
           <input
             className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white mb-4 text-center"
             type="text"
             placeholder="where are you going?"
+            name="destination"
+            onChange={onChangeHandler}
           />
         </label>
 
@@ -24,23 +45,36 @@ const AddTrip = () => {
             className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white mb-4 text-center"
             type="text"
             placeholder="name your trip"
+            onChange={onChangeHandler}
+            name="title"
           />
         </label>
 
-        <label>
+        {/* <label>
           <input
             className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white mb-4 text-center"
             type="text"
             placeholder="trip date: yyyy-mm-dd"
           />
-        </label>
+        </label> */}
 
-        <label>
-          <input
+        <label className="inline-block relative w-64 ">
+          <select
             className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white mb-4 text-center"
-            type="text"
-            placeholder="travel method"
-          />
+            onChange={onChangeHandler}
+            name="travel_method"
+          >
+            <option>Select your travel method</option>
+            <option>airplane</option>
+            <option>car</option>
+            <option>train</option>
+            <option>other</option>
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-1 text-gray-700">
+            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
         </label>
 
         <button
@@ -54,4 +88,8 @@ const AddTrip = () => {
   );
 };
 
-export default AddTrip;
+const mapStateToProps = (state: any) => ({
+  status: state.createTrip.getCreateTripStatus,
+});
+
+export default connect(mapStateToProps, { createTrip })(AddTrip);
