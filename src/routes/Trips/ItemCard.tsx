8632f67d44429ea20868e8bd48c13c75b0d17e53
IT2 <it2@AWT.local>
2020-05-20 14:15:28 -0500

    WIP

---
+++ src/routes/Trips/ItemCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ItemCard = () => {
+    return (
+        <div className='flex'>
+
+            <div>
+                icon
+            </div>
+            <div className='border-b-2 border-gray-600'>
+                    Shampoo
+            </div>
+        </div>
+    )
+}
+
+export default ItemCard;
