09163490cbd0215c8f3ad42c6c73497f2419c09d
IT2 <it2@AWT.local>
2020-05-21 15:25:44 -0500

    text graysout when checked

--- src/routes/Trips/ItemCard.tsx
+++ src/routes/Trips/ItemCard.tsx
@@ -3,16 +3,25 @@ import SVGIcon from '../../components/SVGIcon';
 
 const ItemCard = ({ item }: any) => {
   const [toggle, setToggle] = useState(true);
+  const [textCheck, setTextCheck] = useState('text-gray-800');
 
   const onClickHandler = () => {
-    toggle ? setToggle(false) : setToggle(true);
+    if (toggle) {
+      setToggle(false);
+      setTextCheck('text-gray-500');
+    } else {
+      setToggle(true);
+      setTextCheck('text-gray-800');
+    }
   };
   return (
     <div className="flex items-center h-8">
       <div onClick={onClickHandler} className="w-8 ml-2">
         {toggle ? <SVGIcon icon="NoCheck" /> : <SVGIcon icon="Checked" />}
       </div>
-      <div className="border-b-2 border-gray-200 w-full">{item.label}</div>
+      <div className="border-b-2 w-full">
+        <p className={`${textCheck} text-xl`}>{item.label}</p>
+      </div>
     </div>
   );
 };
