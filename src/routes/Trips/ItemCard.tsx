ff0fcd0c1ec161d8e721b47dc0b88f1cd3b7201b
Pedro Revilla <peterevilla@Pedros-MacBook-Air.local>
2020-05-22 08:40:02 -0500

    hook name fixed

--- src/routes/Trips/ItemCard.tsx
+++ src/routes/Trips/ItemCard.tsx
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 import SVGIcon from '../../components/SVGIcon';
 
 const ItemCard = ({ item }: any) => {
-  const [toggle, setToggle] = useState(true);
+  const [isChecked, toggleChecked] = useState(false);
   const [textCheck, setTextCheck] = useState('text-gray-800');
 
   const onClickHandler = () => {
-    if (toggle) {
-      setToggle(false);
+    if (isChecked) {
+      toggleChecked(false);
       setTextCheck('text-gray-500');
     } else {
-      setToggle(true);
+      toggleChecked(true);
       setTextCheck('text-gray-800');
     }
   };
   return (
     <div className="flex items-center h-8">
       <div onClick={onClickHandler} className="w-8 ml-2">
-        {toggle ? <SVGIcon icon="NoCheck" /> : <SVGIcon icon="Checked" />}
+        {isChecked ? <SVGIcon icon="Checked" /> : <SVGIcon icon="NoCheck" />}
       </div>
       <div className="border-b-2 w-full">
         <p className={`${textCheck} text-xl`}>{item.label}</p>
