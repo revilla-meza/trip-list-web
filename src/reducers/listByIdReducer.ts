c781a0ef6176116485325bda39c59f7cde2a673c
IT2 <it2@AWT.local>
2020-06-01 15:17:14 -0500

    TL-50: empty array added to listByIdReducer when dispatch CREATE_TRIP_SUCCESS

--- src/reducers/listByIdReducer.ts
+++ src/reducers/listByIdReducer.ts
@@ -1,6 +1,7 @@
 import { requestStatus } from '../types/index';
 import { FETCH_LIST_SUCCESS, FETCH_LIST_START, FETCH_LIST_ERROR } from '../actions/fetchList';
-export type ListActions = 'FETCH_LIST_SUCCESS' | 'FETCH_LIST_START' | 'FETCH_LIST_ERROR';
+import { CREATE_TRIP_SUCCESS } from '../actions/createTrip';
+export type ListActions = 'FETCH_LIST_SUCCESS' | 'FETCH_LIST_START' | 'FETCH_LIST_ERROR' | 'CREATE_TRIP_SUCCESS';
 export interface ListState {
   byId: any;
   getListStatus: requestStatus;
@@ -27,6 +28,12 @@ const listByIdReducer = (state = initialState, action: ListAction): ListState =>
       };
     case FETCH_LIST_ERROR:
       return { ...state, getListStatus: requestStatus.error };
+    case CREATE_TRIP_SUCCESS:
+      const listForTrip = { items: [], ...action.payload.list };
+      return {
+        byId: { ...state.byId, [action.payload.listId]: listForTrip },
+        getListStatus: requestStatus.success,
+      };
     default:
       return state;
   }
