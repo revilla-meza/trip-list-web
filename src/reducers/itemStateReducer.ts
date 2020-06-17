ca7621319736e160c225176eadc3e5488f4fc2bb
Luis Revilla <luisrevillameza@gmail.com>
2020-06-17 19:01:11 -0400

    create item state reducer

--- src/reducers/itemStateReducer.ts
+++ src/reducers/itemStateReducer.ts
@@ -29,8 +29,7 @@ interface ListAction {
 }
 
 const initialState = {
-  byId: {},
-  getListStatus: requestStatus.ready,
+  byListId: window.localStorage.getItem('itemStatesByListId') || {},
 };
 
 const listReducer = produce((draft, action: ListAction) => {
@@ -38,36 +37,16 @@ const listReducer = produce((draft, action: ListAction) => {
     return initialState;
   }
   switch (action.type) {
-    case FETCH_LIST_START:
-      draft.getListStatus = requestStatus.loading;
-      break;
-    case FETCH_LIST_SUCCESS:
-      const itemIds = action.payload.items.map((it: { id: any }) => it.id);
-
-      draft.byId[action.payload.id] = {
-        ...action.payload,
-        itemIds,
-      };
-      draft.getListStatus = requestStatus.success;
-      break;
-    case FETCH_LIST_ERROR:
-      draft.getListStatus = requestStatus.error;
-      break;
     case CREATE_TRIP_SUCCESS:
-      const listForTrip = { ...action.payload.list, itemIds: [] };
-      draft.getListStatus = requestStatus.success;
-      draft.byId[action.payload.listId] = listForTrip;
+      draft.byListId[action.payload.listId] = {};
       break;
     case FETCH_ONE_TRIP_SUCCESS:
-      const list = {
-        ...action.payload.list,
-        itemIds: action.payload.list.items.map((it: { id: any }) => it.id),
+      if (!draft.byListId[action.payload.listId]){
+        draft.byListId[action.payload.listId] = {}
       };
-      draft.byId[action.payload.listId] = list;
-      draft.getListStatus = requestStatus.success;
       break;
-    case CREATE_ITEM_SUCCESS:
-      draft.byId[action.payload.listId].itemIds.push(action.payload.id);
+    case SET_ITEM_STATE:
+      draft.byListId[action.payload.listId][action.payload.itemId] = action.payload.state;
       break;
   }
 });
