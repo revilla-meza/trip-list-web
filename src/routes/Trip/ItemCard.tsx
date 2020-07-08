4842f52e2d609018ae422b12c910340945be05c0
Luis Revilla <luisrevillameza@gmail.com>
2020-07-08 15:00:46 -0400

    Toggle state on icon click

--- src/routes/Trip/ItemCard.tsx
+++ src/routes/Trip/ItemCard.tsx
@@ -5,7 +5,7 @@ import SVGIcon from '../../components/SVGIcon';
 const ItemCard = ({ item, isChecked, toggleChecked, deleteItem, itemIds, listId }: any) => {
   const textColor = isChecked ? 'text-gray-500' : 'text-gray-800';
 
-  const onClickHandler = () => {
+  const toggleState = () => {
     if (isChecked) {
       toggleChecked(false);
     } else {
@@ -14,17 +14,17 @@ const ItemCard = ({ item, isChecked, toggleChecked, deleteItem, itemIds, listId 
   };
 
   const defaultOptionsForLongPress = {
-    shouldPreventDefault: true,
+    shouldPreventDefault: false,
     delay: 1000, // delete item after one second pressing.
   };
 
   const onLongPress = () => deleteItem(item.id, listId, itemIds);
 
-  const longPressEvent = useLongPress(onLongPress, onClickHandler, defaultOptionsForLongPress);
+  const longPressEvent = useLongPress(onLongPress, ()=>{}, defaultOptionsForLongPress);
 
   return (
     <div className="p-3 m-2 flex flex-row items-center rounded shadow-md" {...longPressEvent}>
-      <div className="mr-3">{isChecked ? <SVGIcon icon="Checked" /> : <SVGIcon icon="NoCheck" />}</div>
+      <div onClick={toggleState} id="tarjeta" className="mr-3">{isChecked ? <SVGIcon icon="Checked" /> : <SVGIcon icon="NoCheck" />}</div>
       <p className={`${textColor} text-xl`}>{item.label}</p>
     </div>
   );
