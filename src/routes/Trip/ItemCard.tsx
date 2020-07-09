b49abc2e9273fa205f144b7610e6d50a26ee3722
Pedro Revilla <47826392+peterevilla@users.noreply.github.com>
2020-07-09 06:56:55 -0500

    added a confirmation when delete item (#32)
    * added a confirmation when delete item
    
    * added a confirmation when delete item
    
    * added a confirmation when delete item
    
    * I a smaller sixe of box shadow in item cards, looks better

--- src/routes/Trip/ItemCard.tsx
+++ src/routes/Trip/ItemCard.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useLongPress from '../../util/useLongPress';
 import SVGIcon from '../../components/SVGIcon';
 
 const ItemCard = ({ item, isChecked, toggleChecked, deleteItem, itemIds, listId }: any) => {
+  const [itemBeingDeleted, setItemBeingDeleted] = useState(false);
   const textColor = isChecked ? 'text-gray-500' : 'text-gray-800';
 
   const toggleState = () => {
@@ -18,12 +19,31 @@ const ItemCard = ({ item, isChecked, toggleChecked, deleteItem, itemIds, listId 
     delay: 1000, // delete item after one second pressing.
   };
 
-  const onLongPress = () => deleteItem(item.id, listId, itemIds);
+  const onLongPress = () => {
+    setItemBeingDeleted(!itemBeingDeleted);
+  };
+
+  const onDeleteItem = () => deleteItem(item.id, listId, itemIds);
 
   const longPressEvent = useLongPress(onLongPress, null, defaultOptionsForLongPress);
+  if (itemBeingDeleted) {
+    return (
+      <div className="p-3 m-2 bg-red-200 flex flex-row items-center rounded justify-between shadow-sm">
+        <p className=" text-gray-800 text-xl">Delete Item?</p>
+        <div className="flex  mr-3">
+          <button className=" text-green-800 mx-2" onClick={onDeleteItem}>
+            Yes
+          </button>
 
+          <button className=" text-red-800 mx-2" onClick={() => setItemBeingDeleted(!itemBeingDeleted)}>
+            No
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="p-3 m-2 flex flex-row items-center rounded justify-between shadow-md" {...longPressEvent}>
+    <div className="p-3 m-2 flex flex-row items-center rounded justify-between shadow-sm" {...longPressEvent}>
       <p className={`${textColor} text-xl`}>{item.label}</p>
       <div onClick={toggleState} className="mr-3">
         {isChecked ? <SVGIcon icon="Checked" /> : <SVGIcon icon="NoCheck" />}
