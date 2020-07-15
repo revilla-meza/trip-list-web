4609a362f63da0f2a78680440651e68a10db3ac9
Luis Revilla <luisrevillameza@gmail.com>
2020-07-15 13:49:50 -0400

    TL-69: Turn filter into new item input (#35)

--- src/components/FilterItemForm.tsx
+++ src/components/FilterItemForm.tsx
@@ -1,37 +1,55 @@
 import React from 'react';
 import { DebounceInput } from 'react-debounce-input';
+import { connect } from 'react-redux';
+import createItem from '../actions/createItem';
 
 interface ComponentStateProps {
   queryHandler: any;
+  newLabel: boolean;
+  zeroItems: boolean;
+  createItem: any;
+  listId: any;
 }
 
 type AppState = ComponentStateProps;
 
-const FilterItemForm = ({ queryHandler }: AppState) => {
+const FilterItemForm = ({ queryHandler, newLabel, zeroItems, createItem, listId }: AppState) => {
+  const [newItemLabel, setNewItemLabel] = React.useState('');
+
   const onchangeHandler = (e: any) => {
-    queryHandler(e.target.value);
+    const label = e.target.value.trim(); // remove whitespace
+    queryHandler(label);
+    setNewItemLabel(label);
   };
+
   const submitHandler = (e: any) => {
     e.preventDefault();
+    if (newLabel) createItem({ list: listId, label: newItemLabel });
   };
 
   return (
-    <div>
-      <form onSubmit={submitHandler} className="flex items-center h-8">
-        <div className="border-b-2 w-full text-xl">
-          <DebounceInput
-            minLength={1}
-            debounceTimeout={500}
-            className="w-full"
-            name="newItem"
-            type="text"
-            placeholder="    search item"
-            onChange={onchangeHandler}
-          ></DebounceInput>
-        </div>
+    <div className="m-2 border-2 py-2 text-xl">
+      <form onSubmit={submitHandler} className="flex justify-between items-center h-8">
+        <DebounceInput
+          minLength={1}
+          debounceTimeout={500}
+          className="p-2 w-3/4"
+          name="newItem"
+          type="text"
+          placeholder={zeroItems ? 'New item' : 'Search item'}
+          onChange={onchangeHandler}
+        ></DebounceInput>
+        {/*If label is new or list has zero items, we prompt users to add a new item
+          we will update this behavior to prompt for item creation if the list has just a few items that all fit in the viewport (searching isn't 'necessary' in such scenario)
+        */}
+        {(newLabel || zeroItems) && (
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+            ADD
+          </button>
+        )}
       </form>
     </div>
   );
 };
 
-export default FilterItemForm;
+export default connect(null, { createItem })(FilterItemForm);
